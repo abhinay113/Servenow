bb0b01aeb0b9c414ebe7fb9c2e5444f20f32ae90
abhinay113 <jangamabhinai115@gmail.com>
2026-04-19 15:53:18 +0530

    origin changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+const backendUrl = import.meta.env.VITE_API_URL || '/api'
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: backendUrl,
   headers: { 'Content-Type': 'application/json' }
 })
 
